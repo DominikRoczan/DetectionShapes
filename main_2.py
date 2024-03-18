3429e79725a3bb8fd87cc0106f172f36ec9dbf7e
domin <dominik.roczan@gmail.com>
2024-03-18 23:44:32 +0100

    update

--- main_2.py
+++ main_2.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.yaml')
-results = model.train(data='config.yaml', epochs=1)
+# results = model.train(data='C:\\tmp\\dataset\\config.yaml', epochs=1)
+root_config = 'C:\\Users\\domin\\OneDrive\\Pulpit\\Python\\DetectionShapes\\dataset\\config.yaml'
+
+results = model.train(data=root_config, epochs=6)
